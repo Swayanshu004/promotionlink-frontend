cfb4d13c1eddee50d42a0d5d0165d19ae058c591
Swayanshu004 <swayanshusahoo.pri@gmail.com>
2024-11-30 00:09:32 +0530

    landing page resize - phone

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -45,12 +45,12 @@ function Footer() {
     <div className='w-screen '>
         <div className='w-full h-10 bg-violet-700 rounded-t-full'></div>
         <div className='w-full h-24 bg-violet-700 flex items-center justify-between'>
-          <div className='w-5/6 flex items-center lg:pl-20  h-20 text-md font-light '>
+          <div className='w-5/6 flex items-center lg:pl-20 h-20 text-md font-light ml-5'>
             <h3 className='px-2 lg:px-5 py-3 border-2 rounded-3xl hover:bg-violet-600'>
               <span className='font-bold text-violet-950'>@ : </span>swayanshusahoo.pri@gmail.com
             </h3>
           </div>
-          <div className="flex items-center justify-end h-20 w-2/6">
+          <div className="flex items-center justify-end h-20 w-1/6 md:w-2/6">
             <FloatingDock
                 mobileClassName="translate-y-20 z-20 bottom-20 right-5" // only for demo, remove for production
                 items={links}
